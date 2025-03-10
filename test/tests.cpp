7507a868110d896ad422316e5246b4520fb38b0c
Bogdan__ <bogerbloger@yandex.ru>
2025-03-10 19:19:52 +0300

    ггг

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,20 +1,19 @@
-// Copyright 2025 UNN-CS Team
+// Copyright 2023 Your Name or Organization
+
 #include "tasks.h"
 #include "circle.h"
 #include <cmath>
 #include "gtest/gtest.h"
 
-const double PI = 3.141592653589793; 
-
+const double PI = 3.141592653589793;
 
 TEST(CircleTest, SetRadius) {
     Circle c;
     c.setRadius(1.0);
     EXPECT_DOUBLE_EQ(c.getRadius(), 1.0);
-    EXPECT_NEAR(c.getFerence(), 2 * PI, 1e-10); 
-    EXPECT_NEAR(c.getArea(), PI, 1e-10);       
+    EXPECT_NEAR(c.getFerence(), 2 * PI, 1e-10);
+    EXPECT_NEAR(c.getArea(), PI, 1e-10);
 }
-
 
 TEST(CircleTest, SetFerence) {
     Circle c;
@@ -23,14 +22,12 @@ TEST(CircleTest, SetFerence) {
     EXPECT_NEAR(c.getArea(), PI, 1e-10);
 }
 
-
 TEST(CircleTest, SetArea) {
     Circle c;
     c.setArea(PI);
     EXPECT_NEAR(c.getRadius(), 1.0, 1e-10);
     EXPECT_NEAR(c.getFerence(), 2 * PI, 1e-10);
 }
-
 
 TEST(CircleTest, RadiusZero) {
     Circle c;
@@ -40,7 +37,6 @@ TEST(CircleTest, RadiusZero) {
     EXPECT_DOUBLE_EQ(c.getArea(), 0);
 }
 
-
 TEST(CircleTest, LargeRadius) {
     Circle c;
     c.setRadius(1000);
@@ -49,20 +45,18 @@ TEST(CircleTest, LargeRadius) {
     EXPECT_NEAR(c.getArea(), 1000000 * PI, 1e-10);
 }
 
-
 TEST(CircleTest, Zd1) {
     double result = zd_1();
     EXPECT_NEAR(result, 2 * PI * (6378.1 + 0.001), 1e-2);
 }
 
-
 TEST(CircleTest, Zd2) {
     double result = zd_2();
-    double expectedTrackCost = (pow(3 + 1, 2) * PI - PI * 9) * 1000;
+    double expectedTrackCost = (pow(3 + 1, 2) * PI 
+                               - PI * 9) * 1000;
     double expectedBoundCost = 2 * PI * (3 + 1) * 2000;
     EXPECT_NEAR(result, expectedTrackCost + expectedBoundCost, 1e-2);
 }
-
 
 TEST(CircleTest, Zd2RadiusCheck) {
     Circle pool;
@@ -72,20 +66,17 @@ TEST(CircleTest, Zd2RadiusCheck) {
     EXPECT_DOUBLE_EQ(bound.getRadius(), 4);
 }
 
-
 TEST(CircleTest, Zd2AreaCheck) {
     Circle pool;
     pool.setRadius(3);
     EXPECT_NEAR(pool.getArea(), 9 * PI, 1e-10);
 }
 
-
 TEST(CircleTest, Zd2FerenceCheck) {
     Circle bound;
     bound.setRadius(4);
     EXPECT_NEAR(bound.getFerence(), 8 * PI, 1e-10);
 }
-
 
 TEST(CircleTest, Zd2TrackCost) {
     Circle pool;
@@ -94,14 +85,12 @@ TEST(CircleTest, Zd2TrackCost) {
     EXPECT_NEAR(trackArea, 7 * PI, 1e-10);
 }
 
-
 TEST(CircleTest, Zd2BoundCost) {
     Circle bound;
     bound.setRadius(4);
     double boundCost = bound.getFerence() * 2000;
     EXPECT_NEAR(boundCost, 8 * PI * 2000, 1e-10);
 }
-
 
 TEST(CircleTest, Zd2TotalCost) {
     double result = zd_2();
