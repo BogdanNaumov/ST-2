6f450e557b798003da2bfc0c21bd2198ecf9887a
Bogdan__ <bogerbloger@yandex.ru>
2025-06-02 18:06:24 +0300

    add_test

--- test/tests.cpp
+++ test/tests.cpp
@@ -100,3 +100,95 @@ TEST(CircleTest, Zd2TotalCost) {
     double expected = (7 * PI * 1000) + (8 * PI * 2000);
     EXPECT_NEAR(result, expected, 1e-2);
 }
+
+TEST(CircleTest, NegativeRadius) {
+    Circle c;
+    c.setRadius(-5.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), -5.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), -10 * PI);
+    EXPECT_DOUBLE_EQ(c.getArea(), -25 * PI);
+}
+
+TEST(CircleTest, NegativeFerence) {
+    Circle c;
+    c.setFerence(-10 * PI);
+    EXPECT_DOUBLE_EQ(c.getRadius(), -5.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), -25 * PI);
+}
+
+TEST(CircleTest, NegativeArea) {
+    Circle c;
+    c.setArea(-25 * PI);
+    EXPECT_TRUE(std::isnan(c.getRadius()));
+    EXPECT_TRUE(std::isnan(c.getFerence()));
+}
+
+TEST(CircleTest, TinyRadius) {
+    Circle c;
+    c.setRadius(1e-100);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 1e-100);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 2 * PI * 1e-100);
+    EXPECT_DOUBLE_EQ(c.getArea(), PI * 1e-200);
+}
+
+TEST(CircleTest, HugeRadius) {
+    Circle c;
+    c.setRadius(1e100);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 1e100);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 2 * PI * 1e100);
+    EXPECT_DOUBLE_EQ(c.getArea(), PI * 1e200);
+}
+
+TEST(CircleTest, SequentialUpdates) {
+    Circle c;
+    
+    c.setRadius(2.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 2.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 4 * PI);
+    EXPECT_DOUBLE_EQ(c.getArea(), 4 * PI);
+    
+    c.setFerence(6 * PI);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 3.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), 9 * PI);
+    
+    c.setArea(16 * PI);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 4.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 8 * PI);
+}
+
+TEST(CircleTest, ZeroArea) {
+    Circle c;
+    c.setArea(0.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 0.0);
+}
+
+TEST(CircleTest, DoubleUpdate) {
+    Circle c;
+    c.setRadius(10.0);
+    c.setFerence(20 * PI); 
+    
+    EXPECT_DOUBLE_EQ(c.getRadius(), 10.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 20 * PI);
+    EXPECT_DOUBLE_EQ(c.getArea(), 100 * PI);
+}
+
+TEST(CircleTest, Zd1Negative) {
+    Circle tmp;
+    tmp.setRadius(-6378.1);
+    double result = 2 * PI * (-6378.1);
+    EXPECT_DOUBLE_EQ(tmp.getFerence(), result);
+}
+
+TEST(CircleTest, Zd2ZeroRadius) {
+    Circle pool;
+    pool.setRadius(0);
+    
+    Circle bound;
+    bound.setRadius(pool.getRadius() + 1);
+    
+    double track_cost = (pow(1, 2) * PI * 1000;
+    double bound_cost = bound.getFerence() * 2000;
+    
+    EXPECT_DOUBLE_EQ(track_cost + bound_cost, 1000*PI + 4000*PI);
+}
